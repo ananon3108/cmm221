1096726ae8e9f9cbc0984bed678ee8aa88b2f776
ananon3108 <32547583+ananon3108@users.noreply.github.com>
2017-10-07 12:13:21 +0700

    nodejs server + twig

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express') /* กำหนดวิธีการเก็บตัวแปล ประกาศตัวแปร เก็บแล้วไม่สามารถแก้ไขได้เลย เราตั้งชื่อตัวแปรว่า express*/
+    /* เอาตัวแปร express เข้ามาเพื่อเก็บไปไว้ที่ app*/
+const { twig } = require('twig')
+
+const app = express ()
+
+app.set('view engine','twig')
+
+const homeController = require('./controllers/home.controllers')
+const newsController = require('./controllers/news.controllers')
+
+app.get('/', homeController.index)
+app.get('/news', newsController.index)
+
+app.use('/assets', express.static(__dirname+'/static/assets', {
+	maxAge:86400000
+}))
+
+app.listen(8000,function(err) { /* 8000 คือ port ตัวเลข4ตัวที่ดูจากเราทำเซฺฟเวอจำลอง */ 
+	console.log('Server is running on port 8000')
+} )
